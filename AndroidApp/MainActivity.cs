88cb37f3355fda2ea39f4643066f534890d80719
Zulu <jzuluaga55@gmail.com>
2017-05-24 19:30:19 -0500

    Lab 03 Finish

---
+++ AndroidApp/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using SALLab03;
+
+namespace AndroidApp
+{
+    [Activity(Label = "AndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            var Helper = new SharedProject.MySharedCode();
+            new AlertDialog.Builder(this)
+                .SetMessage(Helper.GetFilePath("demo.dat"))
+                .Show();
+
+            Validate();
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+
+        private async void Validate()
+        {
+            var serviceClient = new ServiceClient();
+            var studentEmail = "XXX";
+            var password = "XXX";
+            var myDevice = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+            var result = await serviceClient.ValidateAsync(studentEmail, password, myDevice);
+            var builder = new AlertDialog.Builder(this);
+            var alert = builder.Create();
+            alert.SetTitle("Resultado de la verificación");
+            alert.SetIcon(Resource.Drawable.Icon);
+            alert.SetMessage($"{result.Status}\n{result.Fullname}\n{result.Token}");
+            alert.SetButton("Ok", (s, ev) => { });
+            alert.Show();
+        }
+    }
+}
+
---
+++ SharedProject/MySharedCode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace SharedProject
+{
+    public class MySharedCode
+    {
+        public string GetFilePath(string fileName)
+        {
+#if WINDOWS_UWP
+            var filePath = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, fileName);
+#else
+#if __ANDROID__
+            var libraryPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var filePath = Path.Combine(libraryPath, fileName);
+#endif
+#endif
+            return filePath;
+        }
+    }
+}
